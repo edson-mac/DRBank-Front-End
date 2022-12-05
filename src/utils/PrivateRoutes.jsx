96e704e54fa54ddcb7763070408acf2e830019ac
Edson <edsonmac@Edson-desktop.localdomain>
2022-12-05 13:06:09 -0300

    Changing the redirect

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,10 +1,11 @@
-import { Outlet, Navigate } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { useProvider } from "../provider/provider";
 
 const PrivateRoutes = () => {
+    const navigate = useNavigate();
     const { token } = useProvider();
     return(
-        token ? <Outlet/> : <Navigate to="/"/>
+        token ? <Outlet/> : navigate('/')
     )
 }
 
