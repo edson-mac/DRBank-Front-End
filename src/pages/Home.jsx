150916512c68aef22954c58887116cdd39c20463
Edson <edsonmac@Edson-desktop.localdomain>
2022-12-05 02:21:44 -0300

    logout button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,15 @@ import Input from '../components/input';
 import Button from '../components/button';
 import { motion } from "framer-motion"
 import info from '../assets/info.svg';
+import logout from '../assets/logout.svg';
 import Header from '../components/Header';
 import Swal from 'sweetalert2';
+import { Navigate } from 'react-router-dom';
 
 
 const Home = () => {
 
-  const { token, user } = useProvider();
+  const { token, user, setToken } = useProvider();
   const [balance, setBalance] = useState(0);
   const [depositoValue, setDepositoValue] = useState('');
   const [transferInfo, setTransferInfo] = useState({ cpf: '', value: '' });
@@ -102,6 +104,11 @@ const Home = () => {
     setTransferInfo({ ...transferInfo, [e.target.name]: e.target.value })
   }
 
+  const handleLogout = () => {
+    setToken('');
+  }
+
+
 
 
 
@@ -118,14 +125,22 @@ const Home = () => {
         <div className='flex flex-col mt-56 items-center'>
           <Header />
           <div className='Registerbox mt-4 shadow-xl rounded-3xl border-solid border-4
-     border-green-400 bg-green-300 w-96 text-center content-center'>
-            <h2 className='mt-4 text-2xl'>Seja bem-vindo(a) {user}</h2>
+     border-green-400 bg-green-300 w-96 text-center'>
+            <div className='flex'>
+              <div>
+                <h2 className='ml-20 mt-4 text-2xl'>Bem-vindo(a) {user}</h2>
+              </div>
+              <div>
+                <button onClick={handleLogout}><img className="carLogo w-8 ml-6 mt-6" src={logout} alt="logoutSvg" /></button>
+              </div>
+            </div>
+
             <p className='mt-4 text-3xl'>Saldo: R$ {balance}</p>
 
             <div className='deposito mt-4 flex flex-col ml-auto mr-auto text-center rounded-xl border-2 bg-green-200 border-green-300 border-solid w-8/12'>
               <p className='mt-2 text-xl'>Realizar Depósito</p>
               <div className='mt-2'>
-              <Input name='deposito' type='number' placeholder='Valor do depósito' value={depositoValue} onChange={(e) => setDepositoValue(e.target.value)} />
+                <Input name='deposito' type='number' placeholder='Valor do depósito' value={depositoValue} onChange={(e) => setDepositoValue(e.target.value)} />
               </div>
               <div className='mb-2 mt-2'>
                 <Button name="Depositar" disabled={disableDepositButton} onClick={() => deposit()} />
@@ -134,7 +149,7 @@ const Home = () => {
 
             <div className='mt-5 mb-5 transferencia flex flex-col ml-auto mr-auto text-center rounded-xl border-2 bg-green-200 border-green-300 border-solid w-8/12'>
               <div className='flex flex-raw gap-2 ml-auto mr-auto mt-3'>
-                <p className='text-xl'>Realizar Transferência</p> 
+                <p className='text-xl'>Realizar Transferência</p>
                 <div className="group flex relative">
                   <img className="carLogo w-4" src={info} alt="infoLogo" />
                   <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 
